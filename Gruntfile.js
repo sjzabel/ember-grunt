9167af46c35cf52549c0976815530a1151f27bf8
Stephen J. Zabel <stephen.zabel@q2ebanking.com>
2013-12-20 13:53:36 -0600

    A large initial commit with the larger changes that I made to Sams project

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,148 +4,291 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		jshint: {
-			files: ['gruntfile.js', 'app/controllers/*.js', 'app/*.js', 'app/views/*.js', 'app/routes/*.js'],
-			options: {
-				eqeqeq: true,
-				eqnull: true,
-				latedef: true,
-				undef: true,
-				globalstrict:true,
-				force:true,
-				globals: {
-					jQuery: true,
-					console: true,
-					module: true,
-					document: true,
-					Ember: true,
-					$: true,
-					App: true
+			files: [
+        'gruntfile.js'
+        ,'app/controllers/*.js'
+        ,'app/*.js'
+        ,'app/views/*.js'
+        ,'app/routes/*.js'
+      ]
+			,options: {
+				eqeqeq: true
+				,eqnull: true
+				,latedef: true
+				,undef: true
+				,globalstrict:true
+				,force:true
+				,globals: {
+					jquery: true
+					,console: true
+					,module: true
+					,document: true
+					,ember: true
+					,$: true
+					,app: true
 				}
 			}
-		},
-		concat: {
-			vendor: {
-				src: ['app/library/jquery-1.9.1.js', 'app/library/handlebars-1.0.0.js', 'app/library/ember-1.0.0.js'],
-				dest:'debug/lib.js'
-			},
-			app: {
-				src: ['app/app.js', 'debug/templates.js', 'app/controllers/*.js', 'app/views/*.js', 'app/routes/*.js'],
-				dest:'debug/app.js'
-			},
-			test: {
-				src: ['app/tests/*.js'],
-				dest: 'qunit/tests.js'
-			},
-			testLib: {
-				src:'debug/lib.js',
-				dest:'qunit/lib.js'
-			},
-			testApp: {
-				src:'debug/app.js',
-				dest:'qunit/app.js'
+		}
+		,concat: {
+      lib: {
+				src: [
+          'app/library/jquery-1.9.1.js'
+          ,'app/library/handlebars-1.0.0.js'
+          ,'app/library/ember-1.0.0.js'
+          ,'app/library/ember-data.js'
+          ,'app/library/bootstrap.min.js'
+        ]
+				,dest:'build/lib.js'
 			}
-		},
-		sass: {
+			,libCss: {
+				src: [
+          'app/css/bootstrap-theme.min.css'
+          ,'app/css/bootstrap.min.css'
+          ,'app/css/entypo_sans_sm.css'
+          ,'app/css/animations.css'
+        ]
+				,dest:'build/lib.css'
+			}
+			,app: {
+				src: [
+          'app/app.js'
+          ,'build/templates.js'
+          ,'app/controllers/*.js'
+          ,'app/views/*.js'
+          ,'app/routes/*.js'
+        ]
+				,dest:'build/app.js'
+			}
+			,debugLib: {
+				src:'build/lib.js'
+				,dest:'debug/lib.js'
+			}
+			,debugLibCss: {
+				src:'build/lib.css'
+				,dest:'debug/lib.css'
+			}
+			,debugApp: {
+				src:'build/app.js'
+				,dest:'debug/app.js'
+			}
+			,debugAppCss: {
+				src:'build/app.css'
+				,dest:'debug/app.css'
+			}
+			,test: {
+				src: ['app/tests/*.js']
+				,dest: 'qunit/tests.js'
+			}
+			,testLib: {
+				src:'build/lib.js'
+				,dest:'qunit/lib.js'
+			}
+			,testLibCss: {
+				src:'build/lib.css'
+				,dest:'qunit/lib.css'
+			}
+			,testApp: {
+				src:'build/app.js'
+				,dest:'qunit/app.js'
+			}
+			,testAppCss: {
+				src:'build/app.css'
+				,dest:'qunit/app.css'
+			}
+		}
+		,sass: {
 			css: {
 				files: {
-					'debug/app.css': 'app/css/base.scss'
+					'build/app.css': 'app/css/base.scss'
 				}
 			}
-		},
-		ember_handlebars: {
+		}
+		,ember_handlebars: {
 			compile: {
 				options: {
 					processName: function(fileName) {
-						var arr = fileName.split("."),
-							path = arr[arr.length - 2].split("/"),
-							name = path[path.length - 1];
+						var arr = fileName.split(".")
+							,path = arr[arr.length - 2].split("/")
+							,name = path[path.length - 1];
 						return name;
 					}
-				},
-				files: {
-					"debug/templates.js": "app/templates/*.hbs"
+				}
+				,files: {
+					"build/templates.js": "app/templates/*.hbs"
 				}
 			}
-		},
-		clean: ["debug/images/", "release/images/"],
-		copy: {
-			main: {
-				files: [{
-					expand: true,
-					cwd: 'app/images/',
-					src: ['**'],
-					dest: 'debug/images/'
-				}, {
-					expand: true,
-					cwd: 'app/images/',
-					src: ['**'],
-					dest: 'release/images/'
-				}]
+		}
+		,clean: {
+      build: [
+        "build/"
+      ]
+      ,debug: [
+        "debug/"
+      ]
+      ,release: [
+        "release/"
+      ]
+    }
+		,copy: {
+			debug: {
+				files: [ {
+					expand: true
+					,cwd: 'app/images/'
+					,src: ['**']
+					,dest: 'debug/images/'
+				},{
+					expand: true
+					,cwd: 'app/fonts/'
+					,src: ['**']
+					,dest: 'debug/fonts/'
+				},{
+					expand: false
+					,src: 'app/index/debug.index.html'
+					,dest: 'debug/index.html'
+				} ]
 			}
-		},
-		uglify: {
-			build: {
-				src: ['debug/lib.js','debug/app.js'],
-				dest: 'release/app.min.js'
+      ,release: {
+				files: [ {
+					expand: true
+					,cwd: 'app/images/'
+					,src: ['**']
+					,dest: 'release/images/'
+				},{
+					expand: true
+					,cwd: 'app/fonts/'
+					,src: ['**']
+					,dest: 'release/fonts/'
+				},{
+					expand: false
+					,src: 'app/index/release.index.html'
+					,dest: 'release/index.html'
+				} ]
 			}
-		},
-		cssmin: {
+		}
+		,uglify: {
+			app: {
+				src: [
+          'build/app.js'
+        ]
+				,dest: 'release/app.min.js'
+			}
+			,lib: {
+				src: [
+          'build/lib.js'
+        ]
+				,dest: 'release/lib.min.js'
+			}
+		}
+		,cssmin: {
 			compress: {
 				files: {
-					"release/app.min.css": ["debug/app.css"]
+					"release/app.min.css": ["build/app.css"]
+          ,"release/lib.min.css": ["build/lib.css"]
 				}
 			}
-		},
-		qunit: {
+		}
+		,qunit: {
 			all: {
 				options: {
 					urls: [
 						'http://localhost:9092/index.html'
-					],
-					force:true
+					]
+					,force:true
 				}
 			}
-		},
-		watch: {
+		}
+    ,hashres: {
+      // Global options
+      options: {
+        // Optional. Encoding used to read/write files. Default value 'utf8'
+        encoding: 'ascii',
+        // Optional. Format used to name the files specified in 'files' property.
+        // Default value: '${hash}.${name}.cache.${ext}'
+        fileNameFormat: '${hash}.${name}.cache.${ext}',
+        // Optional. Should files be renamed or only alter the references to the files
+        // Use it with '${name}.${ext}?${hash} to get perfect caching without renaming your files
+        // Default value: true
+        renameFiles: true
+      }
+      // hashres is a multitask. Here 'prod' is the name of the subtask. You can have as many as you want.
+      ,release: {
+        // Specific options, override the global ones
+        options: {
+        // You can override encoding, fileNameFormat or renameFiles
+        }
+        // Files to hash
+        ,src: [
+          // WARNING: These files will be renamed!
+          'release/lib.min.js'
+          ,'release/app.min.js'
+          ,'release/lib.min.css'
+          ,'release/app.min.css'
+        ],
+        // File that refers to above files and needs to be updated with the hashed name
+        dest: 'release/index.html',
+        }
+    }
+		,watch: {
 			scripts: {
-				files: ['app/library/*.js', 'app/*.js', 'app/controllers/*.js', 'app/views/*.js', 'app/routes/*.js', 'app/css/*.scss', 'app/templates/*.hbs', 'app/tests/*.js'],
-				tasks: ['ember_handlebars','concat', 'sass'],
-				options: {
-					debounceDelay: 100
-				}
-			},
-			tests: {
-				files: ['app/tests/*.js'],
-				tasks: ['qunit'],
-				options: {
-					debounceDelay: 100
-				}
-			},
-			images: {
-				files: ['app/images/*'],
-				tasks: ['clean', 'copy'],
-				options: {
+				files: [
+          'app/library/*.js'
+          ,'app/*.js'
+          ,'app/controllers/*.js'
+          ,'app/views/*.js'
+          ,'app/routes/*.js'
+          ,'app/css/*.scss'
+          ,'app/templates/*.hbs'
+          ,'app/tests/*.js'
+        ]
+				,tasks: [
+          'ember_handlebars'
+          ,'concat'
+        ]
+				,options: {
 					debounceDelay: 100
 				}
 			}
-		},
-		connect: {
+			,tests: {
+				files: [
+          'app/tests/*.js'
+        ]
+				,tasks: [
+          'qunit'
+        ]
+				,options: {
+					debounceDelay: 100
+				}
+			}
+			,images: {
+				files: [
+          'app/images/*'
+        ]
+				,tasks: [
+          'clean'
+          ,'copy'
+        ]
+				,options: {
+					debounceDelay: 100
+				}
+			}
+		}
+		,connect: {
 			debug: {
 				options: {
-					port: 9090,
-					base: 'debug'
+					port: 9090
+					,base: 'debug'
 				}
-			},
-			release: {
+			}
+			,release: {
 				options: {
-					port: 9091,
-					base: 'release'
+					port: 9091
+					,base: 'release'
 				}
-			},
-			test: {
+			}
+			,test: {
 				options: {
-					port: 9092,
-					base: 'qunit'
+					port: 9092
+					,base: 'qunit'
 				}
 			}
 		}
@@ -163,7 +306,34 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-hashres');
 
-	grunt.registerTask('default', ['ember_handlebars', 'concat', 'sass', 'clean', 'copy', 'connect', 'watch']);
-	grunt.registerTask('release', ['jshint','uglify', 'cssmin', 'clean', 'copy']);
+	grunt.registerTask('default'
+    ,[
+      'debug'
+      ,'connect'
+      ,'watch'
+    ]);
+	grunt.registerTask('debug'
+    ,[
+      'clean:build'
+      ,'clean:debug'
+      ,'copy:debug'
+      ,'ember_handlebars'
+      ,'sass'
+      ,'concat'
+    ]);
+	grunt.registerTask('release'
+    ,[
+      'jshint'
+      ,'clean:build'
+      ,'clean:release'
+      ,'copy:release'
+      ,'ember_handlebars'
+      ,'sass'
+      ,'concat'
+      ,'uglify'
+      ,'cssmin'
+      //,'hashres'
+    ]);
 };
--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 'use strict';
 var App = Ember.Application.create();
 
+console.log('started');
 App.Router.map(function() {
     this.resource('about');
 });
